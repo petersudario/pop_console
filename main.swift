36f5a77e56a3dd84f7d8553bcf8cee8c08c148a8
petersudario <pedrohesudario@hotmail.com>
2025-03-25 19:01:24 -0300

    Implementar protocolo

--- main.swift
+++ main.swift
@@ -5,3 +5,32 @@
 //  Created by Pedro Henrique Sudario da Silva on 25/03/25.
 //
 
+
+protocol Notificable {
+    var message: String { get set }
+    func sendNotification() -> Void
+}
+
+struct Email: Notificable{
+    var emailAddress: String
+    var message: String
+    func sendNotification() -> Void {
+        print("Enviando email para tal tal tal")
+    }
+}
+
+struct Sms: Notificable {
+    var phoneNumber: String
+}
+
+struct PushNotification: Notificable {
+    let deviceToken: String
+}
+
+extension NotificableExtension: Notificable {
+    func sendNotification -> Void {
+        print("Enviando notificação...")
+    }
+}
+
+
